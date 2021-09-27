dc3c8e36e4ffd2cf852946413bfc84daf497b399
James Cordor <61040004+jamesinaxx@users.noreply.github.com>
2021-09-27 19:48:16 +1000

    Fixed emoji conversion

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,17 @@ const url = (emoji: string): string =>
   `https://twemoji.maxcdn.com/v/13.1.0/svg/${emoji}.svg`;
 
 const parse = (emoji: string): string => {
+  if (emoji.length === 1) {
+    return emoji.charCodeAt(0).toString(16);
+  }
   const comp =
     (emoji.charCodeAt(0) - 0xd800) * 0x400 +
     (emoji.charCodeAt(1) - 0xdc00) +
     0x10000;
   if (comp < 0) {
-    return url(emoji.charCodeAt(0).toString(16));
+    return emoji.charCodeAt(0).toString(16);
   }
-  return url(comp.toString(16));
+  return comp.toString(16);
 };
 
 export interface EmojiProps {
@@ -27,6 +30,6 @@ const Emoji: React.FunctionComponent<EmojiProps> = ({
   emoji,
   width = '1em',
   height = '1em',
-}) => <img src={parse(emoji)} alt={emoji} style={{ width, height }} />;
+}) => <img src={url(parse(emoji))} alt={emoji} style={{ width, height }} />;
 
 export default Emoji;
