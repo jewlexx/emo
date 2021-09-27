4df009eeb6b04cd4a212cfaba7281d9f02a6f2ba
James Cordor <61040004+jamesinaxx@users.noreply.github.com>
2021-09-27 15:11:08 +1000

    Fixed a few things and updated a few things

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,19 @@
+import jsx from 'acorn-jsx';
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
 /**
  * @type {import('rollup').RollupOptions}
  */
 const config = {
+  treeshake: true,
   input: 'src/index.tsx',
   output: {
     dir: 'dist',
     format: 'cjs',
   },
-  plugins: [typescript()],
+  acornInjectPlugins: [jsx()],
+  plugins: [typescript(), terser()],
 };
 
 export default config;
