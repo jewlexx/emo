eb6f41b840ceaf574721d152828ee04ef86739bb
James Cordor <61040004+jamesinaxx@users.noreply.github.com>
2021-09-28 12:39:47 +1000

    Updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,36 @@
 import React from 'react';
 
-const url = (emoji: string): string =>
-  `https://twemoji.maxcdn.com/v/13.1.0/svg/${emoji}.svg`;
+const u = (e: string): string =>
+  `https://twemoji.maxcdn.com/v/latest/svg/${e}.svg`;
 
-const parse = (emoji: string): string => {
-  if (emoji.length === 1) {
-    return emoji.charCodeAt(0).toString(16);
+const p = (e: string): string => {
+  if (e.length === 1) {
+    return e.charCodeAt(0).toString(16);
   }
-  const comp =
-    (emoji.charCodeAt(0) - 0xd800) * 0x400 +
-    (emoji.charCodeAt(1) - 0xdc00) +
-    0x10000;
-  if (comp < 0) {
-    return emoji.charCodeAt(0).toString(16);
-  }
-  return comp.toString(16);
+  const c =
+    (e.charCodeAt(0) - 0xd800) * 0x400 + (e.charCodeAt(1) - 0xdc00) + 0x10000;
+  return (c < 0 ? e.charCodeAt(0) : c).toString(16);
 };
 
 export interface EmojiProps {
   /** The emoji you would like to use. Eg.🕊️ */
   emoji: string;
-  /** The width of the emoji. Will be passed to the width prop in css */
-  width?: string;
-  /** The height of the emoji. Will be passed to the height prop in css */
-  height?: string;
+  /** The font width of the emoji. Will be passed to the width prop in css */
+  fontWidth?: number;
+  /** The font height of the emoji. Will be passed to the height prop in css */
+  fontHeight?: number;
 }
 
 const Emoji: React.FunctionComponent<EmojiProps> = ({
   emoji,
-  width = '1em',
-  height = '1em',
-}) => <img src={url(parse(emoji))} alt={emoji} style={{ width, height }} />;
+  fontWidth = 1,
+  fontHeight = 1,
+}) => (
+  <img
+    src={u(p(emoji))}
+    alt={emoji}
+    style={{ width: `${fontWidth}em`, height: `${fontHeight}em` }}
+  />
+);
 
 export default Emoji;
