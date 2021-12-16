a3fac2dc2b3876d4cefafe43f9fa1518f2db13c0
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-12-16 20:30:51 +1100

    chore: added vue component

---
+++ packages/vue/dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import Emo from '@/emo.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Emo
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <emo />
+  </div>
+</template>
