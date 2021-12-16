5a0fa6cff2b8305fd95e55adaa3fc9c782a6502a
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-12-16 18:31:42 +1100

    chore: removed angular projects for now cuz wtf

--- packages/angular/projects/emo-angular/src/lib/emo-angular.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { EmoAngularComponent } from './emo-angular.component';
-
-@NgModule({
-  declarations: [EmoAngularComponent],
-  imports: [],
-  exports: [EmoAngularComponent],
-})
-export class EmoAngularModule {}
--- packages/angular/projects/emo-angular/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of emo-angular
- */
-
-export * from './lib/emo-angular.component';
-export * from './lib/emo-angular.module';
